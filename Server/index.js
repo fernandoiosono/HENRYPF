e43bba52b4dd50ed70447a6e3bb4210220994360
fernandoiosono <fernandoiosono@gmail.com>
2023-09-27 09:57:59 -0600

    Creación del Boilerplate con Funcionalidad Básica

---
+++ Server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const cors = require('cors');
+const morgan = require('morgan');
+const server = require('./src/server.js');
+const { database } = require('./src/database/database.js')
+
+// const { handlerGames, 
+// 	handlerGenres,
+// 	handlerPlatforms } = require('./src/routes');
+
+const { LOCALHOST_PORT, DB_RESET } = process.env;
+const dbReset = (DB_RESET === "true")
+
+server.use(cors());
+server.use(morgan("dev"));
+
+// server.use('/videogames/games', handlerGames);
+// server.use('/videogames/genres', handlerGenres);
+// server.use('/videogames/platforms', handlerPlatforms);
+
+database.sync({ force: dbReset })
+	.then(() => {
+		server.listen(LOCALHOST_PORT, () => {
+			console.log(`Server raised in port: ${LOCALHOST_PORT}`);
+		});
+	})
+	.catch((error) => console.log(error));
---
+++ Server/src/database/database.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+// const { defineGame, 
+//     defineGenre, 
+//     definePlatform } = require('./models');
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const conn = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+
+const database = new Sequelize(conn, { logging: false });
+
+// defineGame(database);
+// defineGenre(database);
+// definePlatform(database);
+
+// const { Game, Genre, Platform } = database.models;
+
+// Game.belongsToMany(Genre, { 
+//     through: "GameGenre",
+//     foreignKey: "idGame",
+//     otherKey: "idGenre",
+//     timestamps: false
+// });
+
+// Genre.belongsToMany(Game, { 
+//     through: "GameGenre",
+//     foreignKey: "idGenre",
+//     otherKey: "idGame",
+//     timestamps: false
+// });
+
+// Game.belongsToMany(Platform, { 
+//     through: "GamePlatform",
+//     foreignKey: "idGame",
+//     otherKey: "idPlatform",
+//     timestamps: false
+// });
+
+// Platform.belongsToMany(Game, { 
+//     through: "GamePlatform",
+//     foreignKey: "idPlatform",
+//     otherKey: "idGame",
+//     timestamps: false
+// });
+
+module.exports = {
+    database,
+    ...database.models
+};
