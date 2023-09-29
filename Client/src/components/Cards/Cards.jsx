a06307fd5ea18d9d34d9bdd05d49b5c1cb89466d
josepoloadie <jpolo990206@gmail.com>
2023-09-28 21:38:10 -0500

    Se crean los componentes Cards y Card

---
+++ Client/src/components/Cards/Cards.jsx
@@ -0,0 +1,16 @@
+import Card from "../Card/Card";
+import "./Cards.css";
+
+function Cards({ productos }) {
+  return (
+    <div className="card-list">
+      {productos.length > 0
+        ? productos.map((producto) => (
+            <Card key={producto.id} producto={producto} />
+          ))
+        : null}
+    </div>
+  );
+}
+
+export default Cards;
