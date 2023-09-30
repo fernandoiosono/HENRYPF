4aead80e569b52edd081f831145fb9a2207fd1b8
josepoloadie <jpolo990206@gmail.com>
2023-09-30 18:50:36 -0500

    Se modifica la estructura y el orden del componente Card

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
-
 import "./Card.css";
+import img_prueba from "../../assets/img/producto/ejemplo.png";
 
 function Card({ producto }) {
-  const { id, nombre, imagen, precio } = producto;
+  const { idProducto, nombre, imagen, precio } = producto;
 
   return (
-    <div className="card-container">
-      <p>{id}</p>
-      <Link>
-        <img className="img" src={imagen} alt={nombre} />
-      </Link>
-      <h2>{nombre}</h2>
-      <p>{precio}</p>
+    <div className="card">
+      <div className="imgBox">
+        <Link to={`/detalle/${idProducto}`}>
+          <img src={imagen} alt={nombre} className="img" />
+        </Link>
+      </div>
+      <div className="contentBox">
+        <h3>{nombre}</h3>
+        <h2 className="price">
+          <p>${precio}</p>
+        </h2>
+        <p>
+          <Link className="buy" to={`/detalle/${idProducto}`}>
+            Comprar
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
