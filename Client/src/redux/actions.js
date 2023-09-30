d1c6e270ddedb8b10f045225c603d8c495e1d1ec
josepoloadie <jpolo990206@gmail.com>
2023-09-30 18:02:29 -0500

    Se queman datos en las actions para validar funcionamiento

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,56 +1,62 @@
-import axios from 'axios';
+import axios from "axios";
 import { TRAER_PRODUCTOS, SET_PAGINA, BUSCAR_PRUDUCTOS } from "./actions_types";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import productos from "../../../productos.json";
 
-const URL = 'http://localhost:3001/';
+const URL = "http://localhost:3001/";
 
 export const traerProductos = () => {
-    try {
-        return async (dispatch) => {
-            const { data } = await axios.get(`${URL}productos`);  //! VERIFICAR RUTA CON EL BACK
-            return dispatch({
-                type: TRAER_PRODUCTOS,
-                payload: data
-            })
-        }
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    return async (dispatch) => {
+      // const { data } = await axios.get(`${URL}productos`);  //! VERIFICAR RUTA CON EL BACK
+      const data = productos;
+      return dispatch({
+        type: TRAER_PRODUCTOS,
+        payload: data,
+      });
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const setPagina = (pagina) => {
-    try {
-        return (dispatch) => {
-            return dispatch({
-                type: SET_PAGINA,
-                payload: pagina
-            })
-        }
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    return (dispatch) => {
+      return dispatch({
+        type: SET_PAGINA,
+        payload: pagina,
+      });
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const buscarPruductos = (nombre) => {
-    try {
-        return (dispatch) => {
-            const allProductos = useSelector(state => state.allProductos);
-            const num = Number(nombre);
-            if (!isNaN(num)) {
-                const resultado = allProductos.filter(producto => producto.idProducto === num)
-                return dispatch({
-                    type: SET_PAGINA,
-                    payload: resultado
-                })
-            } else {
-                const resultado = allProductos.filter(producto => producto.nombre.toLowerCase().includes(nombre.toLowerCase()))
-                return dispatch({
-                    type: SET_PAGINA,
-                    payload: resultado
-                })
-            }
-        }
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    return (dispatch) => {
+      const allProductos = useSelector((state) => state.allProductos);
+      const num = Number(nombre);
+      if (!isNaN(num)) {
+        const resultado = allProductos.filter(
+          (producto) => producto.idProducto === num
+        );
+        return dispatch({
+          type: SET_PAGINA,
+          payload: resultado,
+        });
+      } else {
+        const resultado = allProductos.filter((producto) =>
+          producto.nombre.toLowerCase().includes(nombre.toLowerCase())
+        );
+        return dispatch({
+          type: SET_PAGINA,
+          payload: resultado,
+        });
+      }
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
