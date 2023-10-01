4d36f05de3d339e1fbe8ee11124316306b8744bc
fernandoiosono <fernandoiosono@gmail.com>
2023-10-01 09:03:50 -0600

    Routes Handlers Added

---
+++ Server/src/routes/index.js
@@ -0,0 +1,15 @@
+const handlerCards = require('./handlerCards.js');
+const handlerCategories = require('./handlerCategories.js');
+const handlerOrders = require('./handlerOrders.js');
+const handlerProducts = require('./handlerProducts.js');
+const handlerUsers = require('./handlerUsers.js');
+const handlerUserTypes = require('./handlerUserTypes.js');
+
+module.exports = {
+    handlerCards,
+    handlerUsers,
+    handlerOrders,
+    handlerProducts,
+    handlerUserTypes,
+    handlerCategories
+};
