177783a096837d6d6e4cf7b5ec7d4387589efff4
josepoloadie <jpolo990206@gmail.com>
2023-10-01 22:35:11 -0500

    Se crea el caso para realizar el ordenamiento de los productos y el filtrado

--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -1,38 +1,96 @@
-import { TRAER_PRODUCTOS, SET_PAGINA, BUSCAR_PRUDUCTOS } from "./actions_types";
+import {
+  TRAER_PRODUCTOS,
+  SET_PAGINA,
+  BUSCAR_PRUDUCTOS,
+  OBTENER_CATEGORIAS,
+  FILTER_CATEGORIA,
+  SET_ORDER,
+} from "./actions_types";
 // import * as viewCaption from "../views/viewCaptions.js";
 
 const initialState = {
-	allProductos: [],
-	productosEnc: [],
-	carrito: [],
-	inicioSesion: true,
-	pagina: 1
+  allProductos: [],
+  productosMostrar: [],
+  productosEnc: [],
+  carrito: [],
+  inicioSesion: true,
+  pagina: 1,
+  categorias: [
+    //Borrar estos datos cuando se tenga conexion con el Backend
+    {
+      id: 1,
+      name: "Nutricion deportiva",
+    },
+    {
+      id: 2,
+      name: "Proteina",
+    },
+    {
+      id: 3,
+      name: "Aminoacidos",
+    },
+    {
+      id: 4,
+      name: "Equipamiento",
+    },
+    {
+      id: 5,
+      name: "Ropa deportiva",
+    },
+  ],
 };
 
 const rootReducer = (state = initialState, { type, payload }) => {
-	switch (type) {
+  switch (type) {
+    case TRAER_PRODUCTOS:
+      return {
+        ...state,
+        allProductos: payload,
+        productosMostrar: payload,
+      };
 
-		case TRAER_PRODUCTOS:
-			return {
-				...state,
-				allProductos: payload
-			};
+    case SET_PAGINA:
+      return {
+        ...state,
+        pagina: payload,
+      };
 
-		case SET_PAGINA:
-			return {
-				...state,
-				pagina: payload
-			};
+    case BUSCAR_PRUDUCTOS:
+      return {
+        ...state,
+        productosEnc: payload,
+      };
 
-		case BUSCAR_PRUDUCTOS:
-			return {
-				...state,
-				productosEnc: payload
-			};
+    case OBTENER_CATEGORIAS:
+      return {
+        ...state,
+        categorias: payload,
+      };
 
-		default:
-			return { ...state };
-	};
+    case FILTER_CATEGORIA:
+      return {
+        ...state,
+        filterByCategory: payload,
+      };
+
+    case SET_ORDER:
+      let orderedProductos = [...state.productosMostrar];
+      let ordered;
+      if (payload === "Ascendente") {
+        ordered = orderedProductos.sort((a, b) => a.precio - b.precio);
+      } else if (payload === "Descendente") {
+        ordered = orderedProductos.sort((a, b) => b.precio - a.precio);
+      } else if (payload === "Ordenar") {
+        ordered = [...state.allProductos];
+      }
+      return {
+        ...state,
+        productosMostrar: ordered,
+      };
+
+    default:
+      return { ...state };
+  }
 };
 
 export default rootReducer;
