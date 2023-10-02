f45717e0a9140038b717987497e5c2f4b27471c4
fernandoiosono <fernandoiosono@gmail.com>
2023-10-02 16:40:06 -0600

    Role's Controllers Base Code Tested

--- Server/src/routes/handlerRoles.js
+++ Server/src/routes/handlerRoles.js
@@ -4,11 +4,9 @@ const errorHandler = require('../middlewares');
 const { getRoles } = require('../controllers');
 
 router.get('/all', errorHandler(async (req, res) => {
-    // const roles = await getRoles();
+    const roles = await getRoles();
 
-    // res.status(200).json(roles);
-
-    res.status(200).send('getRoles');
+    res.status(200).json(roles);
 }));
 
 module.exports = router;
