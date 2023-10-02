c76558ea8b8ce893f22c39cdcb1bf3d56f781fb6
fernandoiosono <fernandoiosono@gmail.com>
2023-10-02 16:51:38 -0600

    Order's Controllers Base Code Tested

--- Server/src/routes/handlerOrders.js
+++ Server/src/routes/handlerOrders.js
@@ -7,38 +7,30 @@ const { getOrderByID,
     postOrder } = require('../controllers');
 
 router.get('/all', errorHandler(async (req, res) => {
-    // const orders = await getOrders();
+    const orders = await getOrders();
 
-    // res.status(200).json(orders);
-
-    res.status(200).send('getOrders');
+    res.status(200).json(orders);
 }));
 
 router.get('/user', errorHandler(async (req, res) => {
-    // const { idUser } = req.query;
-    // const orders = await getOrdersByUser(idUser);
+    const { idUser } = req.query;
+    const orders = await getOrdersByUser(idUser);
 
-    // res.status(200).json(orders);
-
-    res.status(200).send('getOrdersByUser');
+    res.status(200).json(orders);
 }));
 
 router.get('/:id', errorHandler(async (req, res) => {
-    // const { id } = req.params;
-    // const order = await getOrderByID(id);
+    const { id } = req.params;
+    const order = await getOrderByID(id);
 
-    // res.status(200).json(order);
-
-    res.status(200).send('getOrderByID');
+    res.status(200).json(order);
 }));
 
 router.post('/', errorHandler(async (req, res) => {
-    // const newOrder = req.body;
-    // const orderCreated = await postOrder(newOrder);
+    const newOrder = req.body;
+    const orderCreated = await postOrder(newOrder);
 
-    // res.status(200).json(orderCreated);
-
-    res.status(200).send('postOrder');
+    res.status(200).json(orderCreated);
 }));
 
 module.exports = router;
