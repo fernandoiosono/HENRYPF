755a747c47de03051c97148ec50179e07d998eb4
fernandoiosono <fernandoiosono@gmail.com>
2023-10-03 03:21:12 -0600

    getProducts Controller's Correction

--- Server/src/controllers/Products/getProducts.js
+++ Server/src/controllers/Products/getProducts.js
@@ -1,42 +1,16 @@
-require('dotenv').config();
-const axios = require('axios');
-const { Product, Category } = require('../../database/database.js');
+require("dotenv").config();
+const axios = require("axios");
+const { Product, Category } = require("../../database/database.js");
 
-// const { LAPI_URL_PRODUCTS } = process.env;
-
-const mapProduct = (e) => ({ 
-    idProduct: e.idProducto, 
-    name: e.nombre, 
-    imageURL: e.imagen,
-    description: e.descripcion,
-    price: e.precio,
-    stock: e.stock,
-    discount: e.descuento,
-    active: e.active,
- }); 
-  
- const saveProducts = async () => { 
-   try { 
-     const response = await axios.get('http://localhost:5000/products');
-     const productos = response.data ? response.data.map(mapProduct) : []; 
-     const result = await Product.bulkCreate(productos, { ignoreDuplicates: true }); 
-     console.log('Product save:', result.length);
-   } catch (error) { 
-    console.error('Error making the request:', error); 
-   } 
- }; 
+const { LAPI_URL_PRODUCTS } = process.env;
 
 const getProducts = async () => {
-        const products = await Product.findAll({ 
-          attributes: ['idProduct', 'name', 'imageURL', 'description', 'price', 'stock', 'discount', 'active'], 
-          include: { 
-            model: Category, 
-            attributes: ["name"] 
-          } 
-        }); return products;
+	const productsDB = await Product.findAll();
+    const { data } = await axios.get(LAPI_URL_PRODUCTS);
+
+    if (!productsDB.length) await Product.bulkCreate(data);
+
+	return data;
 };
 
-saveProducts();
-
-module.exports =
-getProducts;
+module.exports = getProducts;
