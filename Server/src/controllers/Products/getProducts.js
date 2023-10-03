e1b4da925b3829720fbd5a49f767d969dc650929
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-03 00:39:30 -0600

    RutaGetProducts

--- Server/src/controllers/Products/getProducts.js
+++ Server/src/controllers/Products/getProducts.js
@@ -1,11 +1,42 @@
 require('dotenv').config();
 const axios = require('axios');
-const { Product } = require('../../database/database.js');
+const { Product, Category } = require('../../database/database.js');
 
-const { LAPI_URL_PRODUCTS } = process.env;
+// const { LAPI_URL_PRODUCTS } = process.env;
+
+const mapProduct = (e) => ({ 
+    idProduct: e.idProducto, 
+    name: e.nombre, 
+    imageURL: e.imagen,
+    description: e.descripcion,
+    price: e.precio,
+    stock: e.stock,
+    discount: e.descuento,
+    active: e.active,
+ }); 
+  
+ const saveProducts = async () => { 
+   try { 
+     const response = await axios.get('http://localhost:5000/products');
+     const productos = response.data ? response.data.map(mapProduct) : []; 
+     const result = await Product.bulkCreate(productos, { ignoreDuplicates: true }); 
+     console.log('Product save:', result.length);
+   } catch (error) { 
+    console.error('Error making the request:', error); 
+   } 
+ }; 
 
 const getProducts = async () => {
-    return "getProductsController";
+        const products = await Product.findAll({ 
+          attributes: ['idProduct', 'name', 'imageURL', 'description', 'price', 'stock', 'discount', 'active'], 
+          include: { 
+            model: Category, 
+            attributes: ["name"] 
+          } 
+        }); return products;
 };
 
-module.exports = getProducts;
+saveProducts();
+
+module.exports =
+getProducts;
--- Server/src/database/models/Product.js
+++ Server/src/database/models/Product.js
@@ -1,52 +1,45 @@
-const { DataTypes } = require('sequelize'); 
-module.exports = (database) => { 
-   // defino el modelo 
-   database.define('Product', { 
-  
-    idProduct: { 
-        type: DataTypes.INTEGER, 
-        primaryKey: true, 
-        autoIncrement: true, 
-        allowNull: true 
-      },  
-  
-    name: { 
-        type: DataTypes.STRING, 
-        allowNull: false, 
-      }, 
+const { DataTypes: dtype } = require("sequelize");
 
-    image: { 
-        type:DataTypes.STRING, 
-        allowNull: false 
-      }, 
-
-    description: { 
-        type:DataTypes.TEXT, 
-        allowNull: false 
-      }, 
-  
-    price: { 
-        type:DataTypes.INTEGER, 
-        allowNull: false 
-      }, 
-  
-    stock: { 
-        type: DataTypes.INTEGER, 
-        default: 0, 
-      }, 
-  
-    state: { 
-        type: DataTypes.ENUM("Active"), 
-        allowNull: false 
-      },
-
-    discount: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-  
- }, {
-  timestamps: false
- }
- ); 
- };
+module.exports = (database) => {
+	database.define(
+		"Product",
+		{
+			idProduct: {
+				type: dtype.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			name: {
+				type: dtype.STRING,
+				allowNull: false
+			},
+			imageURL: {
+				type: dtype.TEXT,
+				allowNull: false
+			},
+			description: {
+				type: dtype.TEXT,
+				allowNull: false
+			},
+			price: {
+				type: dtype.INTEGER,
+				allowNull: false
+			},
+			stock: {
+				type: dtype.INTEGER,
+				default: 0
+			},
+			discount: {
+				type: dtype.INTEGER,
+				default: 0
+			},
+			active: {
+				type: dtype.BOOLEAN,
+				default: true
+            },
+		},
+		{
+			timestamps: false
+		}
+	);
+};
