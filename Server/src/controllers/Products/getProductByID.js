63265876c944d82ae600883c07f85c8eaff81cf7
fernandoiosono <fernandoiosono@gmail.com>
2023-10-03 15:13:18 -0600

    getProductByID Now Shows Also Category Info

--- Server/src/controllers/Products/getProductByID.js
+++ Server/src/controllers/Products/getProductByID.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const axios = require('axios');
-const { Product } = require('../../database/database.js');
+const { Product, Category } = require('../../database/database.js');
 
 const { LAPI_URL_PRODUCTS } = process.env;
 
 const getProductByID = async (id) => {
     const product = await Product.findOne({ 
-        where: { idProduct: id } 
+        where: { 
+            idProduct: id 
+        },
+        attributes: {
+            exclude: 'CategoryIdCategory'
+        },
+        include: [{
+            model: Category,
+            attributes: [ 'idCategory', 'name' ]
+        }]
+        
     });
 
     if (!product) throw new Error("The Product with the ID Entered Doesn't Exist");
