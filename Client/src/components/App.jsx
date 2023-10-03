866703d13995d43fc65e12ccfb031fd52cdd9d64
josepoloadie <jpolo990206@gmail.com>
2023-10-02 21:28:09 -0500

    Se modifica

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -6,57 +6,54 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { traerProductos } from "../redux/actions";
-import { 
-	Landing, 
-	Home, 
-	Catalogo, 
-	Acceso, 
-	About, 
-	Carrito, 
-	Detail, 
-	Pago, 
-	Registro, 
-	CatalogoAdmin, 
-	EdicionProducto } from "../views";
-
+import {
+  Landing,
+  Home,
+  Catalogo,
+  Acceso,
+  About,
+  Carrito,
+  Detail,
+  Pago,
+  Registro,
+  CatalogoAdmin,
+  EdicionProducto,
+} from "../views";
 
 const App = () => {
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
 
-	const { pathname } = useLocation();
-	const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(traerProductos());
+  }, []);
 
-	useEffect(() => {
-		dispatch(traerProductos())
-	},[])
-
-
-	return (
-		<SectionApp>
-			<main>
-				{pathname!=='/'&&<Nav/>}
-				<Routes>
-					<Route path="/" element={<Landing />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/catalogo" element={<Catalogo />} />
-					<Route path="/acceso" element={<Acceso />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/carrito" element={<Carrito />} />
-					<Route path="/detalle/:id" element={<Detail />} />
-					<Route path="/pago" element={<Pago />} />
-					<Route path="/registro" element={<Registro />} />
-					<Route path="/catalogoAdmin" element={<CatalogoAdmin />} />
-					<Route path="/edicion" element={<EdicionProducto />} />
-
-
-				</Routes>
-			</main>
-		</SectionApp>
-	)
+  return (
+    <SectionApp>
+      <main>
+        {pathname !== "/" && <Nav />}
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/acceso" element={<Acceso />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/detalle/:id" element={<Detail />} />
+          <Route path="/pago" element={<Pago />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/catalogoAdmin" element={<CatalogoAdmin />} />
+          <Route path="/detalleAdmin/:id" element={<Detail />} />
+          <Route path="/edicion" element={<EdicionProducto />} />
+        </Routes>
+      </main>
+    </SectionApp>
+  );
 };
 
 const SectionApp = styled.section`
-	height: calc(100vh - 40px);
-	padding:20px;
+  height: calc(100vh - 40px);
+  padding: 20px;
 `;
 
 export default App;
--- Client/src/views/CatalogoAdmin/CatalogoAdmin.jsx
+++ Client/src/views/CatalogoAdmin/CatalogoAdmin.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import List from "../../components/List/List";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Filter from "../../components/Filter/Filter.jsx";
+import Ordering from "../../components/Ordering/Ordering.jsx";
+import Pagination from "../../components/Pagination/pagination.components.jsx";
+const CatalogoAdmin = () => {
+  const traerProductos = useSelector((state) => state.productosMostrar);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardForPage] = useState(25);
 
-const CatalogoAdmin = ()=>{
-    return (
-        <>
-        <h1>Esta es la vista del Catalogo de Admin</h1>
-        </>
-    );
+  const ultimoIndiceProducto = currentPage * cardForPage;
+  const primerIndiceProducto = ultimoIndiceProducto - cardForPage;
+  const currentProductos = traerProductos.slice(
+    primerIndiceProducto,
+    ultimoIndiceProducto
+  );
+
+  const paged = function (pageNumber) {
+    setCurrentPage(pageNumber);
+  };
+  return (
+    <>
+      <Filter />
+      <Ordering />
+      <List productos={currentProductos} />
+      <Pagination
+        cardForPage={cardForPage}
+        productos={traerProductos.length}
+        paged={paged}
+      />
+    </>
+  );
 };
 
 export default CatalogoAdmin;
