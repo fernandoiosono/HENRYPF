7d599d1c07e0f1995c3ae1987249d7f724f0b5e3
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-03 05:42:59 -0600

    getProductsByName Controller's

--- Server/src/controllers/Products/getProductsByName.js
+++ Server/src/controllers/Products/getProductsByName.js
@@ -5,7 +5,18 @@ const { Product } = require('../../database/database.js');
 const { LAPI_URL_PRODUCTS } = process.env;
 
 const getProductsByName = async (name) => {
-    return name;
+
+    const product = await Product.findOne({
+        where: { name },
+        attributes: ['name', 'description'],
+    });
+    
+    if (product) {
+        return product;
+    } else {
+    const { data } = await axios.get(LAPI_URL_PRODUCTS);
+        return data.find((product) => product.name === name);
+    }
 };
 
 module.exports = getProductsByName;
