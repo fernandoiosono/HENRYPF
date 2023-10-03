e499eb6f1f89d1b5f1dd17a0db20e53a5eb7cc67
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-03 04:32:40 -0600

    getProductByID Controller's

--- Server/src/controllers/Products/getProductByID.js
+++ Server/src/controllers/Products/getProductByID.js
@@ -4,8 +4,21 @@ const { Product } = require('../../database/database.js');
 
 const { LAPI_URL_PRODUCTS } = process.env;
 
-const getProductByID = async (id) => {
-    return id;
+const getProductByID = async (idProduct) => {
+
+    const productsDB = await Product.findOne({ where: { idProduct } });
+
+    const { data } = await axios.get(LAPI_URL_PRODUCTS);
+
+    if (productsDB) {
+        return productsDB;
+    }
+    if (!productsDB) {
+        return null;
+    }
+   
+    return { data, productsDB };
+     // Devuelve los datos recuperados de la API y el registro de la base de datos.
 };
 
 module.exports = getProductByID;
