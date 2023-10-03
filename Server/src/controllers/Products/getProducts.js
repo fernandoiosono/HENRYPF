5de95a10389ab73b69a2265c7d9312303542968d
fernandoiosono <fernandoiosono@gmail.com>
2023-10-03 14:43:05 -0600

    getProducts Tested & Charged / Also Includes Category Data

--- Server/src/controllers/Products/getProducts.js
+++ Server/src/controllers/Products/getProducts.js
@@ -5,12 +5,28 @@ const { Product, Category } = require("../../database/database.js");
 const { LAPI_URL_PRODUCTS } = process.env;
 
 const getProducts = async () => {
-	const productsDB = await Product.findAll();
-    const { data } = await axios.get(LAPI_URL_PRODUCTS);
+    // First we verify that the products are loaded in the database
+	const productsDB = await Product.findOne();
 
-    if (!productsDB.length) await Product.bulkCreate(data);
+    // And if not, we load them
+    if (!productsDB) {
+        const { data } = await axios.get(LAPI_URL_PRODUCTS);
 
-	return data;
+        await Product.bulkCreate(data);
+    }
+
+    // This way we can always return the content of the database
+    const products = await Product.findAll({
+        attributes: {
+            exclude: 'CategoryIdCategory'
+        },
+        include: [{
+            model: Category,
+            attributes: [ 'idCategory', 'name' ]
+        }]
+    });
+
+	return products;
 };
 
 module.exports = getProducts;
