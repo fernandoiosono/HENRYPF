6ece84286e1fb7d0ebae8316a9fde5101c414342
josepoloadie <jpolo990206@gmail.com>
2023-10-03 08:03:57 -0500

    Se modifica la escructura de la vista admin

--- Client/src/views/CatalogoAdmin/CatalogoAdmin.jsx
+++ Client/src/views/CatalogoAdmin/CatalogoAdmin.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+// import React from "react";
+// import List from "../../components/List/List";
+// import { useState } from "react";
+// import { useSelector } from "react-redux";
+// import Pagination from "../../components/Pagination/pagination.components.jsx";
+// import Aside from "../../components/Aside/Aside";
+// import "./CatalogoAdmin.css";
+
+// const CatalogoAdmin = () => {
+//   const traerProductos = useSelector((state) => state.productosMostrar);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [cardForPage] = useState(15);
+//   const ultimoIndiceProducto = currentPage * cardForPage;
+//   const primerIndiceProducto = ultimoIndiceProducto - cardForPage;
+//   const currentProductos = traerProductos.slice(
+//     primerIndiceProducto,
+//     ultimoIndiceProducto
+//   );
+//   const paged = function (pageNumber) {
+//     setCurrentPage(pageNumber);
+//   };
+//   return (
+//     <>
+//       <div className="main-container">
+//         <Aside />
+//         <List productos={currentProductos} />
+//       </div>
+//       <Pagination
+//         cardForPage={cardForPage}
+//         productos={traerProductos.length}
+//         paged={paged}
+//       />
+//     </>
+//   );
+// };
+
+// export default CatalogoAdmin;
+
+import React, { useState } from "react";
 import List from "../../components/List/List";
-import { useState } from "react";
+import CrearProducto from "../../components/CrearProducto/CrearProducto";
 import { useSelector } from "react-redux";
-import Filter from "../../components/Filter/Filter.jsx";
-import Ordering from "../../components/Ordering/Ordering.jsx";
 import Pagination from "../../components/Pagination/pagination.components.jsx";
+import Aside from "../../components/Aside/Aside";
+import "./CatalogoAdmin.css";
+
 const CatalogoAdmin = () => {
   const traerProductos = useSelector((state) => state.productosMostrar);
   const [currentPage, setCurrentPage] = useState(1);
-  const [cardForPage] = useState(25);
-
+  const [cardForPage] = useState(15);
+  const [mostrarVerProductos, setMostrarVerProductos] = useState(false); // Nuevo estado para controlar qué componente mostrar
   const ultimoIndiceProducto = currentPage * cardForPage;
   const primerIndiceProducto = ultimoIndiceProducto - cardForPage;
   const currentProductos = traerProductos.slice(
@@ -20,16 +59,36 @@ const CatalogoAdmin = () => {
   const paged = function (pageNumber) {
     setCurrentPage(pageNumber);
   };
+
+  const mostrarVerProductosHandler = () => {
+    setMostrarVerProductos(true);
+  };
+
+  const mostrarCrearProductosHandler = () => {
+    setMostrarVerProductos(false);
+  };
+
   return (
     <>
-      <Filter />
-      <Ordering />
-      <List productos={currentProductos} />
-      <Pagination
-        cardForPage={cardForPage}
-        productos={traerProductos.length}
-        paged={paged}
-      />
+      <div className="main-container">
+        <Aside
+          mostrarVerProductos={mostrarVerProductos}
+          mostrarVerProductosHandler={mostrarVerProductosHandler}
+          mostrarCrearProductosHandler={mostrarCrearProductosHandler}
+        />
+        {mostrarVerProductos ? (
+          <List productos={currentProductos} />
+        ) : (
+          <CrearProducto /> // Renderiza el componente CrearProductos cuando mostrarVerProductos es falso
+        )}
+      </div>
+      {mostrarVerProductos ? (
+        <Pagination
+          cardForPage={cardForPage}
+          productos={traerProductos.length}
+          paged={paged}
+        />
+      ) : null}
     </>
   );
 };
