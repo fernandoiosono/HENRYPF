f6f015295bf0dfb98120a1c49fe98dcb260e94d0
fernandoiosono <fernandoiosono@gmail.com>
2023-10-03 12:34:01 -0600

    getProductByID Correction

--- Server/src/controllers/Products/getProductByID.js
+++ Server/src/controllers/Products/getProductByID.js
@@ -4,21 +4,14 @@ const { Product } = require('../../database/database.js');
 
 const { LAPI_URL_PRODUCTS } = process.env;
 
-const getProductByID = async (idProduct) => {
+const getProductByID = async (id) => {
+    const product = await Product.findOne({ 
+        where: { idProduct: id } 
+    });
 
-    const productsDB = await Product.findOne({ where: { idProduct } });
+    if (!product) throw new Error("The Product with the ID Entered Doesn't Exist");
 
-    const { data } = await axios.get(LAPI_URL_PRODUCTS);
-
-    if (productsDB) {
-        return productsDB;
-    }
-    if (!productsDB) {
-        return null;
-    }
-   
-    return { data, productsDB };
-     // Devuelve los datos recuperados de la API y el registro de la base de datos.
+    return product;
 };
 
 module.exports = getProductByID;
