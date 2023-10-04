396cb9322e067889d9c3015b3cd92d0a0aacdf0c
fernandoiosono <fernandoiosono@gmail.com>
2023-10-04 01:38:13 -0600

    postProduct Now Works with Helpers

--- Server/src/controllers/Products/postProduct.js
+++ Server/src/controllers/Products/postProduct.js
@@ -1,16 +1,12 @@
 require('dotenv').config();
 const { Product } = require('../../database/database.js');
-const newProductDataIsValid = require('../../helpers/newProductDataIsValid.js')
+
+const { newProductDataIsValid,
+    getNewIDProduct } = require('../../helpers');
 
 const postProduct = async (newProduct) => {
     if (await newProductDataIsValid(newProduct)) {
-        const lastProduct = await Product.findOne({
-            order: [['idProduct', 'DESC']]
-        });
-
-        const idProduct = lastProduct.idProduct + 1;
-
-        newProduct.idProduct = idProduct;
+        newProduct.idProduct = await getNewIDProduct();
 
         const productCreated = await Product.create(newProduct)
 
