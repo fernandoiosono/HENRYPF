7154a684a9a44d7c6321618bd531cbcd39646b54
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-05 20:26:04 -0600

    patchProduct y modificacion en archivo handlerProducts

--- Server/src/controllers/Products/patchProduct.js
+++ Server/src/controllers/Products/patchProduct.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
-const axios = require('axios');
 const { Product } = require('../../database/database.js');
 
-const { LAPI_URL_PRODUCTS } = process.env;
 
-const patchProduct = async (newData) => {
-    return newData;
+const patchProduct = async (idProduct, newData) => {
+        
+        const product = await Product.findOne({ where: { idProduct }});
+
+        for (const key in newData) {
+            if (newData.hasOwnProperty(key)) {
+                product[key] = newData[key];
+            }
+        }
+       
+            await product.save();
+            
+
+    if (!product)  throw new Error('Error modifying the product');
+
+    return product;
+    
 };
 
 module.exports = patchProduct;
