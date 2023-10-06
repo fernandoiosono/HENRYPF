79f4f548b43db537e4891faeda8745bafac10675
fernandoiosono <fernandoiosono@gmail.com>
2023-10-06 01:26:28 -0600

    getNewIDProduct Helper Deleted / Deprecated After Change Product ID to UUID

--- Server/src/helpers/index.js
+++ Server/src/helpers/index.js
@@ -1,7 +1,5 @@
 const newProductDataIsValid = require("./Products/newProductDataIsValid.js");
-const getNewIDProduct = require("./Products/getNewIDProduct.js");
 
 module.exports = {
-    newProductDataIsValid,
-    getNewIDProduct
+    newProductDataIsValid
 };
