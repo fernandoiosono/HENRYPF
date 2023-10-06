79a587237630e4e2213ff9348a172a02ea3e3ca7
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-05 21:53:36 -0600

    putActivateProduct modificacion en el manejo de errores

--- Server/src/controllers/Products/putActivateProduct.js
+++ Server/src/controllers/Products/putActivateProduct.js
@@ -4,10 +4,12 @@ const { Product } = require('../../database/database.js');
 const putActivateProduct = async (idProduct, activate) => {
     const product = await Product.findOne({ where: { idProduct }});
 
+    
+    if (!product) throw new Error( 'Producto no encontrado' );
+
     product.active = activate === "true";
     await product.save();
 
-    if (!product) throw new Error( 'Producto no encontrado' );
     
     return product;
 };
