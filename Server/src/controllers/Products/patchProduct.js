dc37ded532992f4e9c6f92795a3c802026942d2f
fernandoiosono <fernandoiosono@gmail.com>
2023-10-06 13:48:37 -0600

    Minor Indentation Changes / Save Method Replaced by Update Method

--- Server/src/controllers/Products/patchProduct.js
+++ Server/src/controllers/Products/patchProduct.js
@@ -1,28 +1,23 @@
 require('dotenv').config();
 const { Product } = require('../../database/database.js');
 
-
 const patchProduct = async (idProduct, newData) => {
-        
-        const product = await Product.findOne({ where: { idProduct }});
+    const product = await Product.findOne({ where: { idProduct }});
 
-        if (!product) throw new Error('Error modifying the product');
+    if (!product) throw new Error("The Product You Are Trying to Update Doesn't Exist!");
 
-        let hasChanges = false;
+    let hasChanges = false;
 
-        for (const key in newData) {
-            if (newData.hasOwnProperty(key) && product[key] !== newData[key]) {
-                product[key] = newData[key];
-                hasChanges = true;
-            }
+    for (const key in newData) {
+        if (newData.hasOwnProperty(key) && product[key] !== newData[key]) {
+            product[key] = newData[key];
+            hasChanges = true;
         }
-    
-        if (hasChanges) {
-            await product.save();
-        }
-    
-        return product;
-    
+    }
+
+    if (hasChanges) await product.update();
+
+    return product;
 };
 
 module.exports = patchProduct;
