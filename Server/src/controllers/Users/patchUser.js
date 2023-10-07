98850cb543c900abaec2c05d3199a90cd651acff
ManuelE <apamanuel@gmail.com>
2023-10-07 17:52:30 -0400

    Creacion del controlador patchUser  y se modifico el handler de la ruta

--- Server/src/controllers/Users/patchUser.js
+++ Server/src/controllers/Users/patchUser.js
@@ -5,7 +5,28 @@ const { User } = require('../../database/database.js');
 const { LAPI_URL_USERS } = process.env;
 
 const patchUser = async (newData) => {
-    return newData;
+    const [updatedRowsCount, [updatedUser]] = await User.update(
+            {
+                imageURL: newData.imageURL !== undefined ? newData.imageURL : User.imageURL,
+                active: newData.active !== undefined ? newData.active : User.active,
+                isAdmin: newData.isAdmin !== undefined ? newData.isAdmin : User.isAdmin,
+            },
+            {
+                where: { idUser: newData.idUser },
+                returning: true,
+            }
+        );
+
+    if(updatedRowsCount === 0) throw new Error('Usuario no encontrado');
+
+    return {
+        idUser: updatedUser.idUser,
+        nickName: updatedUser.nickName,
+        email: updatedUser.email,
+        imageURL: updatedUser.imageURL,
+        active: updatedUser.active,
+        isAdmin: updatedUser.isAdmin,
+    };
 };
 
 module.exports = patchUser;
