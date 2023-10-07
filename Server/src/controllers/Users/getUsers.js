e5272521e5389db6448059941c2167e63aac1c2e
ManuelE <apamanuel@gmail.com>
2023-10-07 16:49:26 -0400

    Modificacion del controlador getUsers

--- Server/src/controllers/Users/getUsers.js
+++ Server/src/controllers/Users/getUsers.js
@@ -4,9 +4,22 @@ const { User } = require('../../database/database.js');
 const getUsers = async () => {
     const users = await User.findAll();
 
-    if (!users.length) throw new Error("There's No Users in the Database!");
+    if (!users.length) {
+        throw new Error("There's No Users in the Database!");
+    } else {
+        newUsers = users.map(user=>{
+            return {
+                idUser: user.idUser,
+                nickName: user.nickName,
+                email: user.email,
+                imageURL: user.imageURL,
+                active: user.active,
+                isAdmin: user.isAdmin,
+            }
+        })
+    }
 
-    return users;
+    return newUsers;
 };
 
 module.exports = getUsers;
