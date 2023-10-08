7a9b1dab709684d054da89b661f12655f3496db9
fernandoiosono <fernandoiosono@gmail.com>
2023-10-08 17:12:10 -0600

    ShoppingCart Controllers Creation

--- Server/src/controllers/ShoppingCart/getCartByUserID.js
+++ Server/src/controllers/ShoppingCart/getCartByUserID.js
@@ -1,7 +1,23 @@
-const { ShoppingCart } = require("../../database/database.js");
+const { ShoppingCart, Product, User } = require("../../database/database.js");
 
 const getCartByUserID = async (idUser) => {
-    return idUser;
+    const cart = await User.findAll({
+        where: { idUser: idUser },
+        attributes: [],
+        include: [{
+            model: Product,
+            as: "products",
+            through: {
+                model: ShoppingCart,
+                attributes: []
+            },
+            attributes: {
+                exclude: ['description', 'CategoryIdCategory']
+            }
+        }]
+    });
+    
+    return cart[0].products;
 };
 
 module.exports = getCartByUserID;
--- Server/src/controllers/ShoppingCart/postCartByUserID.js
+++ Server/src/controllers/ShoppingCart/postCartByUserID.js
@@ -1,7 +1,44 @@
-const { ShoppingCart } = require("../../database/database.js");
+const { ShoppingCart, Product, User } = require("../../database/database.js");
 
-const postCartByUserID = async (idUser, products) => {
-    return idUser;
+const postCartByUserID = async (idUser, localProducts) => {
+    // Obtenemos los productos existentes en la BD
+    const dbProducts = await ShoppingCart.findAll({
+        where: { idUser: idUser }
+    });
+
+    // Agregamos a la BD productos nuevos en el carrito
+    for (let x = 0; x < localProducts.length; x++) {
+        const [ product, created ] = await ShoppingCart.findOrCreate({
+            where: { idUser: idUser, idProduct: localProducts[x] },
+            defaults: { idUser: idUser, idProduct: localProducts[x] }
+        });
+    };
+
+    // Eliminamos de la BD productos que ya no estén en el carrito
+    for (let x = 0; x < dbProducts.length; x++) {
+        if (!localProducts.includes(dbProducts[x].idProduct)) {
+            dbProducts[x].destroy();
+        }
+    };
+
+    // Devolvemos los productos actualizados de la BD
+    const updatedCart = await User.findAll({
+        where: { idUser: idUser },
+        attributes: [],
+        include: [{
+            model: Product,
+            as: "products",
+            through: {
+                model: ShoppingCart,
+                attributes: []
+            },
+            attributes: {
+                exclude: ['CategoryIdCategory']
+            }
+        }]
+    });
+
+    return updatedCart[0].products;
 };
 
 module.exports = postCartByUserID;
