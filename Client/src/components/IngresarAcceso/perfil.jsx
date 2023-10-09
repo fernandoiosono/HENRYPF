a29ad39056335fc4c1baab2324a18c58dca1ab0f
LuisaFGO <fgiraldo070@gmail.com>
2023-10-08 20:50:32 -0500

    Perfil con nuevos valores

--- Client/src/components/IngresarAcceso/perfil.jsx
+++ Client/src/components/IngresarAcceso/perfil.jsx
@@ -1,59 +1,69 @@
 import React from "react";
 import LogoutButton from "../IngresarAcceso/logout";
-import SubirImagen from '../Cloudinary/cloudinary.component'
-import { useDispatch, useSelector} from "react-redux";
+import SubirImagen from "../Cloudinary/cloudinary.component";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import {crearUsuario} from "../../redux/actions";
+import { crearUsuario } from "../../redux/actions";
 import "./perfil.css";
 
-const Perfil = ({user, autenticado}) => {
+const Perfil = ({ user, autenticado }) => {
   const dispatch = useDispatch();
   const usuario = useSelector((state) => state.usuario);
 
-   const newUsuario = {
+  const newUsuario = {
     idAuth0: user.sub,
-    nickName : user.nickname,
-    email : user.email,
+    name: user.name,
+    nickName: user.nickname,
+    email: user.email,
     imageURL: user.picture,
-   }
+  };
 
-   useEffect(() => {
-    if(autenticado){
-    dispatch(crearUsuario(newUsuario));}
-  },[]);
-  
-    // useEffect(()=>{
-    //     if(isAuthenticated){
-    //         dispatch(accionverificarrol)
-    //     }
-    // }, [isAuthenticated]);
+  useEffect(() => {
+      if (autenticado) {
+      dispatch(crearUsuario(newUsuario));
+    }
+  }, []);
 
-    // useEffect(()=>{
-    //     if(administrador){
-    //         redirect, window.
-    //     }
-    // }, [rol]);
+  // useEffect(()=>{
+  //     if(isAuthenticated){
+  //         dispatch(accionverificarrol)
+  //     }
+  // }, [isAuthenticated]);
+
+  // useEffect(()=>{
+  //     if(administrador){
+  //         redirect, window.
+  //     }
+  // }, [rol]);
 
   return (
-      <div className="perfil">
-        <img src={usuario.data.imageURL} alt="imagen del usuario" className="imagen" />
-        <div className="cloudinary">
-          <h3 className="cambiarImg">Cambiar Imagen:</h3>
-          < SubirImagen/>
-        </div>
-        <div className="div_txt">
-          <h1 className="titulo">Bienvenido {usuario.data.nickName}</h1>
-          <h3 className="txt">Nombre</h3>
-          <p className="txt_info">{usuario.data.email}</p>
-          <h3 className="txt">Nombre de usuario</h3>
-          <p className="txt_info">{usuario.data.nickName}</p>
-          <h3 className="txt">Correo</h3>
-          <p className="txt_info">{usuario.data.email}</p>
-          <div className="btn">
-            <LogoutButton />
+    <>
+      {usuario.data ? (
+        <div className="perfil">
+          <img
+            src={usuario.data.imageURL}
+            alt="imagen del usuario"
+            className="imagen"
+          />
+          <div className="cloudinary">
+            <h3 className="cambiarImg">Cambiar Imagen:</h3>
+            <SubirImagen />
+          </div>
+          <div className="div_txt">
+            <h1 className="titulo">Bienvenido {usuario.data.nickName}</h1>
+            <h3 className="txt">Nombre</h3>
+            <p className="txt_info">{usuario.data.name}</p>
+            <h3 className="txt">Nombre de usuario</h3>
+            <p className="txt_info">{usuario.data.nickName}</p>
+            <h3 className="txt">Correo</h3>
+            <p className="txt_info">{usuario.data.email}</p>
+            <div className="btn">
+              <LogoutButton />
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
+    </>
   );
 };
 
