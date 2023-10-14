2a59d6a0be548f04ee3bc9107350edf39131e34d
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-13 21:06:10 -0600

    deploy

--- Server/src/server.js
+++ Server/src/server.js
@@ -18,7 +18,7 @@ server.use(morgan("dev"));
  server.use(express.json()); 
  server.use(cors()); 
  server.use((req, res, next) => { 
-     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from 
+     res.header('Access-Control-Allow-Origin', 'https://henrypf-production-d333.up.railway.app/'); // update to match the domain you will make the request from 
      res.header('Access-Control-Allow-Credentials', 'true'); 
      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
