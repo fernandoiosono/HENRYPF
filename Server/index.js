6cf9a0829c7e15678a279feaafa32a34578d773f
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-14 06:47:27 -0600

    deploy

--- Server/index.js
+++ Server/index.js
@@ -23,13 +23,13 @@ server.use(morgan("dev"));
 
 // server.name = 'API'; 
 
- server.use((req, res, next) => { 
-     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from 
-     res.header('Access-Control-Allow-Credentials', 'true'); 
-     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
-     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
-     next(); 
- }); 
+//  server.use((req, res, next) => { 
+//      res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from 
+//      res.header('Access-Control-Allow-Credentials', 'true'); 
+//      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
+//      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
+//      next(); 
+//  }); 
 
 server.use('/moveon/users', handlerUsers);
 server.use('/moveon/cards', handlerCards);
