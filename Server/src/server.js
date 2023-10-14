3279f731ba7260ae297220eacc51f8f8f19d590c
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-13 20:50:41 -0600

    deploy

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,7 +1,28 @@
-const express = require('express');
+// const express = require('express');
+
+// const server = express();
+
+// server.use(express.json());
+
+// module.exports = server;
+
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const server = express();
 
-server.use(express.json());
+server.name = 'API'; 
+
+server.use(morgan("dev")); 
+ server.use(express.json()); 
+ server.use(cors()); 
+ server.use((req, res, next) => { 
+     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from 
+     res.header('Access-Control-Allow-Credentials', 'true'); 
+     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
+     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
+     next(); 
+ }); 
 
 module.exports = server;
