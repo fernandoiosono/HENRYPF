81e614201895aa4044cd4a0274de382a1c649142
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-15 17:41:51 -0600

    deployfront2

--- Server/index.js
+++ Server/index.js
@@ -21,15 +21,15 @@ const dbReset = (DB_RESET === "true");
 server.use(cors());
 server.use(morgan("dev"));
 
-// server.name = 'API'; 
+server.name = 'API'; 
 
-//  server.use((req, res, next) => { 
-//      res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from 
-//      res.header('Access-Control-Allow-Credentials', 'true'); 
-//      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
-//      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
-//      next(); 
-//  }); 
+ server.use((req, res, next) => { 
+     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from 
+     res.header('Access-Control-Allow-Credentials', 'true'); 
+     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
+     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
+     next(); 
+ }); 
 
 server.use('/moveon/users', handlerUsers);
 server.use('/moveon/cards', handlerCards);
