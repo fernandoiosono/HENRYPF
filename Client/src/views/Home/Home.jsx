436f638c815ad852c496c41e05eb1d5a9a0788ca
isdavid92 <isdavid92@hotmail.com>
2023-10-15 19:36:31 -0500

    carrito listo

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import style from './Home.module.css';
 import botonBlanco from '../../assets/img/home/botonProductosBlanco.png';
 import { useNavigate } from "react-router-dom";
-import imagenFondo from '../../assets/img/home/FotoPrincipalHome.png'
+import imagenFondo from '../../assets/img/home/FotoPrincipalHome.png';
+import imgTesti1 from '../../assets/img/home/Testimonio1.jpg';
+import imgTesti2 from '../../assets/img/home/Testimonio2.jpg';
+import imgTesti3 from '../../assets/img/home/Testimonio3.jpg';
 
 const Home = ()=>{
 
@@ -27,6 +30,27 @@ const Home = ()=>{
                     <h1 className={style.productos}>PRODUCTOS</h1>
                 </div>
                 <h3 className={style.notaBoton}>Adelgazar | Aumento masa muscular</h3>
+            </div>
+            <div className={style.divTestimonios}>
+                <h3 className={style.testimonios}>TESTIMONIOS</h3>
+                <div className={style.divTestimonio1}>
+                    <h3 className={style.testimonio1}>
+                        "Cada vez tengo más fuerza, ahora puedo hacer una rutina completa"
+                    </h3>
+                    <img src={imgTesti1} className={style.imgtestimonio1}/>
+                </div>
+                <div className={style.divTestimonio2}>
+                    <img src={imgTesti2} className={style.imgtestimonio2}/>
+                    <h3 className={style.testimonio2}>
+                        "Llevo 6 meses utilizando las mancuernas y siguen como nuevas"
+                    </h3>
+                </div>
+                <div className={style.divTestimonio3}>
+                    <h3 className={style.testimonio3}>
+                        "Llevo 6 meses utilizando las mancuernas y siguen como nuevas"
+                    </h3>
+                    <img src={imgTesti3} className={style.imgtestimonio3}/>
+                </div>
             </div>
         </div>
     );
