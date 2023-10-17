1316b5c8ed4f80981d7a72e71dbb06ca268fd078
LuisaFGO <fgiraldo070@gmail.com>
2023-10-16 22:30:49 -0500

    se agrega el logo con dirección a home

--- Client/src/views/Landing/Landing.jsx
+++ Client/src/views/Landing/Landing.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+// import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import logoOff from "../../assets/img/logoAntes/logoAntes.png";
+import style from "./Landing.module.css";
 
-const Landing = ()=>{
-    return (
-        <>
-        <h1>Esta es la vista del Landing</h1>
-        </>
-    );
+const Landing = () => {
+  const navigate = useNavigate();
+  const handlerBtn =()=>{
+    navigate("/home");
+  }
+  return (
+    <>
+        {/* <NavLink to="/home"> */}
+          <div className={style.img_logo}>
+           <img src={logoOff} alt="logoOff" className={style.logoOff} onClick={handlerBtn}/>
+          </div>
+        {/* </NavLink> */}
+    </>
+  );
 };
 
 export default Landing;
-
