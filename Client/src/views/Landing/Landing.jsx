ba4cde981527c4f0ccdc178a47ef9094c9989d0e
LuisaFGO <fgiraldo070@gmail.com>
2023-10-17 18:55:39 -0500

    Agregando cambio de logo, texto

--- Client/src/views/Landing/Landing.jsx
+++ Client/src/views/Landing/Landing.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import logoOff from "../../assets/img/logoAntes/logoAntes.png";
+import logoOn from "../../assets/img/logo/logo.png";
+import fondo from "../../assets/img/landing/fondoLanding.jpg"
 import style from "./Landing.module.css";
 
 const Landing = () => {
   const navigate = useNavigate();
+  const [logo, setLogo] = useState(logoOff); 
+  const [background, setBackground] = useState(""); 
+
   const handlerBtn =()=>{
-    navigate("/home");
+    setLogo(logoOn);
+    setBackground(fondo);
+    setTimeout(() => { 
+      navigate("/home"); }, 2000); 
   }
+
+  useEffect(() => { 
+    document.body.style.backgroundImage = background;
+   }, [background]); 
+
   return (
     <>
-        {/* <NavLink to="/home"> */}
           <div className={style.img_logo}>
-           <img src={logoOff} alt="logoOff" className={style.logoOff} onClick={handlerBtn}/>
+           <img src={logo} alt="logo" className={style.logo} onClick={handlerBtn}/>
+           <h1 className={style.txt_tienda}>Tienda virtual</h1>
           </div>
-        {/* </NavLink> */}
     </>
   );
 };
