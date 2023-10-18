4612d88c2d4b9ff6e8a1763bf0265ca55c3a28e3
ivloa6919@gmail.com <ivloa6919@gmail.com>
2023-10-17 19:25:18 -0600

    cambios para crear producto

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ const dbReset = (DB_RESET === "true");
 
 server.use(cors(
 	{
-		origin: "https://henrypf-production-c75d.up.railway.app/acceso", // Reemplaza con el dominio de tu frontend
+		origin: "https://127.0.0.1.3000", // Reemplaza con el dominio de tu frontend
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true, // Si deseas permitir cookies y autenticación
 	  }
