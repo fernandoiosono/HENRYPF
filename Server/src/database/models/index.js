fa982ea680ea0a4d3ec77fb64b7e0dcc70202059
fernandoiosono <fernandoiosono@gmail.com>
2023-10-22 10:25:29 -0600

    Exporting Models Definition Modified

--- Server/src/database/models/index.js
+++ Server/src/database/models/index.js
@@ -1,15 +1,13 @@
-const defineCard = require("./Card.js");
 const defineCategory = require("./Category.js");
 const defineOrder = require("./Order.js");
+const defineOrderProduct = require("./OrderProduct.js");
 const defineProduct = require("./Product.js");
-const defineShoppingCart = require("./ShoppingCart.js");
 const defineUser = require("./User.js");
 
 module.exports = {
     defineUser,
-    defineCard,
     defineOrder,
     defineProduct,
-    defineShoppingCart,
-    defineCategory
+    defineCategory,
+    defineOrderProduct
 };
