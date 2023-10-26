d1c08f6b1ff8a1a17ed098a7c6f399b643a12563
fernandoiosono <fernandoiosono@gmail.com>
2023-10-25 21:11:10 -0600

    getOrders Controller Finished

--- Server/src/controllers/Orders/getOrders.js
+++ Server/src/controllers/Orders/getOrders.js
@@ -1,7 +1,24 @@
-const { Order } = require('../../database/database.js');
+const { Op } = require('sequelize');
+const { User, Order } = require('../../database/database.js');
+const { LOADED } = require('../../helpers/Orders/orderStatus.js');
 
 const getActiveOrders = async () => {
-    const orders = await Order.findAll();
+    const orders = await Order.findAll({
+        where: {
+            status: {
+                [Op.not]: LOADED
+            }
+        },
+        attributes: [
+            "idOrder",
+            "amount",
+            "status"
+        ],
+        include: {
+            model: User,
+            attributes: ["nickName"]
+        }
+    });
 
     return orders;
 };
