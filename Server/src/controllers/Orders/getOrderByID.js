e527279907c723ffffdd30831a64a42ac6e280df
fernandoiosono <fernandoiosono@gmail.com>
2023-10-26 01:33:40 -0600

    getOrderByID Controller Finished

--- Server/src/controllers/Orders/getOrderByID.js
+++ Server/src/controllers/Orders/getOrderByID.js
@@ -1,10 +1,29 @@
-const { Order } = require('../../database/database.js');
+const { Order, User, Product, OrderProduct } = require('../../database/database.js');
 
 const getOrderByID = async (idOrder) => {
     const order = await Order.findOne({
         where: {
             idOrder: idOrder
-        }
+        },
+        attributes: {
+            exclude: ["idOrder", "status", "UserIdUser"]
+        },
+        include: [
+            {
+                model: User,
+                attributes: ["name", "email"]
+            },
+            {
+                model: Product,
+                as: "products",
+                attributes: ["name", "imageURL"],
+                through: {
+                    model: OrderProduct,
+                    as: "shoppingCart",
+                    attributes: ["quantity"]
+                }
+            }
+        ]
     });
 
     return order;
